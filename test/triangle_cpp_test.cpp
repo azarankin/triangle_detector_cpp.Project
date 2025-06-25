04c7ee10e12dfc20f23f12313b6dd71afdfe7b2a
azarankin <w3arthur@gmail.com>
2025-06-25 22:40:00 +0300

    refactoring unit tests

--- test/triangle_cpp_test.cpp
+++ test/triangle_cpp_test.cpp
@@ -1,91 +1,61 @@
-
 #include <gtest/gtest.h>
 #include <opencv2/opencv.hpp>
-#include <iostream>
-#include <string>
 #include <filesystem>
+#include <string>
 #include "../shape_detector_cpp.h"
 
-
 namespace fs = std::filesystem;
-fs::path source_file = __FILE__;
-fs::path dir = source_file.parent_path();
 
-
-fs::path REFERENCE_INPUT = dir / "reference_input";
-const std::string triangle_img = "triangle.png";
-const std::string other_triangle_img = "other_triangle.png";
-fs::path triangle_img_path = REFERENCE_INPUT / triangle_img;
-fs::path other_triangle_img_path = REFERENCE_INPUT / other_triangle_img;
-
-fs::path REFERENCE_OUTPUT = dir / "reference_output";
-const std::string triangle_contours_img = "triangle_contours.png";
-const std::string other_triangle_contours_img = "other_triangle_contours.png";
-cv::Mat expected_triangle_img = cv::imread(fs::path(REFERENCE_OUTPUT / triangle_img), cv::IMREAD_COLOR);
-cv::Mat expected_triangle_contours_img = cv::imread(fs::path(REFERENCE_OUTPUT / triangle_contours_img), cv::IMREAD_COLOR);
-cv::Mat expected_other_triangle_contours_img = cv::imread(fs::path(REFERENCE_OUTPUT / other_triangle_contours_img), cv::IMREAD_COLOR);
-
-fs::path CURRENT_OUTPUT = dir / "current_output";
-fs::path new_triangle_img_path = CURRENT_OUTPUT / triangle_img;
-
-
-
-TEST(SimpleTest, BasicCheck) {
-    ASSERT_EQ(1 + 1, 2);
-}
-// Check if file do exist
-
-void image_similarity_asserts(const cv::Mat& expected, const cv::Mat& result)
+class TriangleImageTest : public ::testing::Test 
 {
-    ASSERT_FALSE(expected.empty()) << "Reference image is missing!";
+protected:
+    fs::path dir = fs::path(__FILE__).parent_path();
+    fs::path reference_input = dir / "reference_input";
+    fs::path reference_output = dir / "reference_output";
+    fs::path current_output = dir / "current_output";
 
-    // First check if the sizes and the type is the same
-    ASSERT_EQ(result.size(), expected.size()) << "Result image output is not same size";
-    ASSERT_EQ(result.type(), expected.type()) << "Result image output is not same type";
+    fs::path triangle_img = reference_input / "triangle.png";
+    fs::path other_triangle_img = reference_input / "other_triangle.png";
+    fs::path expected_triangle_img = reference_output / "triangle.png";
+    fs::path expected_triangle_contours_img = reference_output / "triangle_contours.png";
+    fs::path expected_other_triangle_contours_img = reference_output / "other_triangle_contours.png";
+    fs::path new_triangle_img = current_output / "triangle.png";
 
-    // Calc the differences (Norm)
-    double diff = cv::norm(result, expected, cv::NORM_L2);
-    // 0 < 1  e-6 is good enogh range
-    ASSERT_LT(diff, 1e-6) << "Result image output is not similar enough!";
+    // Helper function for image similarity check
+    void image_similarity_asserts(const cv::Mat& expected, const cv::Mat& result, double tol = 1e-6)
+    {
+        ASSERT_FALSE(expected.empty()) << "Reference image is missing!";
+        ASSERT_FALSE(result.empty()) << "Result image is empty!";
+        ASSERT_EQ(result.size(), expected.size()) << "Image sizes differ";
+        ASSERT_EQ(result.type(), expected.type()) << "Image types differ";
+        double diff = cv::norm(result, expected, cv::NORM_L2);
+        ASSERT_LT(diff, tol) << "Images are not similar enough! Diff: " << diff;
+    }
+};
+
+TEST_F(TriangleImageTest, GenerateTriangle) 
+{
+    create_triangle_image(new_triangle_img);
+    cv::Mat expected = cv::imread(this->expected_triangle_img, cv::IMREAD_COLOR);
+    cv::Mat result = cv::imread(new_triangle_img, cv::IMREAD_COLOR);
+    image_similarity_asserts(expected, result);
 }
 
-
-TEST(TriangleImageTest, GenerateSampleTriangle) 
+TEST_F(TriangleImageTest, DetectTriangleContour) 
 {
-    
-    create_triangle_image(new_triangle_img_path); 
-    cv::Mat result = cv::imread(new_triangle_img_path, cv::IMREAD_COLOR);
-
-    image_similarity_asserts(expected_triangle_img, result);
-}
-
-TEST(TriangleImageTest, CheckTriangleCountour) 
-{
-    auto template_contour = find_shape_contour(triangle_img_path);
-
-    cv::Mat triangle_image = cv::imread(triangle_img_path, cv::IMREAD_COLOR);
+    auto template_contour = find_shape_contour(triangle_img);
+    cv::Mat triangle_image = cv::imread(triangle_img, cv::IMREAD_COLOR);
     auto result = draw_contour_on_image(triangle_image, template_contour);
-
-    //cv::imwrite(fs::path(CURRENT_OUTPUT / triangle_img), result);
-    image_similarity_asserts(expected_triangle_contours_img, result);
+    cv::Mat expected = cv::imread(expected_triangle_contours_img, cv::IMREAD_COLOR);
+    image_similarity_asserts(expected, result);
 }
 
-
-
-
-
-TEST(TriangleImageTest, ApplyTriangleCountoursOnAnother) 
+TEST_F(TriangleImageTest, DetectTriangleContourOnAnother) 
 {
-    auto template_contour = find_shape_contour(triangle_img_path);
-
-    cv::Mat triangle_image = cv::imread(triangle_img_path, cv::IMREAD_COLOR);
-
-    cv::Mat other_triangle_image = cv::imread(other_triangle_img_path, cv::IMREAD_COLOR);
-    //imshow("", other_triangle_frame_img);
+    auto template_contour = find_shape_contour(triangle_img);
+    cv::Mat other_triangle_image = cv::imread(other_triangle_img, cv::IMREAD_COLOR);
     auto detected_contours = contour_compare(other_triangle_image, template_contour);
     auto result = draw_contour_on_image(other_triangle_image, detected_contours);
-    
-    image_similarity_asserts(expected_other_triangle_contours_img, result);
+    cv::Mat expected = cv::imread(expected_other_triangle_contours_img, cv::IMREAD_COLOR);
+    image_similarity_asserts(expected, result);
 }
-
-
